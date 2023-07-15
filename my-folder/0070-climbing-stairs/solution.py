9da8450aafccb6521441fdfda29c31ab5dcc6037
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-07-15 19:48:57 +0000

    [LeetCode Sync] Runtime - 56 ms (5.74%), Memory - 16.2 MB (99.90%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        memo[0] = 0
-        memo[1] = 1
-        memo[2] = 2
-        for i in range(3,n+1):
-            memo[i] = memo[i-1] + memo[i-2]
-        return memo[n]
+        cache = {}
+        cache[n] = cache[n-1] = 1
+        for i in range(n-2,-1,-1):
+            cache[i] = cache[i+1] + cache[i+2]
+        return cache[0]
+        
