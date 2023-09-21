cff50fc270d53dbd9b53fdad30005530efee6e5b
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-09-21 04:48:41 +0000

    [LeetCode Sync] Runtime - 251 ms (9.38%), Memory - 20.4 MB (83.43%)

---
+++ my-folder/1055-pairs-of-songs-with-total-durations-divisible-by-60/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        remainders = collections.defaultdict(int)
+        count = 0
+        for t in time:
+            if t%60 == 0:
+                count += remainders[0]
+            else:
+                count += remainders[60 - (t % 60)]
+            remainders[t % 60] += 1
+        return count
+
+        
