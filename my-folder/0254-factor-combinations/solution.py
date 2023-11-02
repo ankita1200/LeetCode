e2c4db042a2b63ffb9a2c255fa6eadec008342fc
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-11-02 16:13:57 +0000

    [LeetCode Sync] Runtime - 75 ms (63.92%), Memory - 17.4 MB (82.18%)

---
+++ my-folder/0254-factor-combinations/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getFactors(self, n):
+        if n == 1: return []
+
+        def backtrack(n,idx,path):
+            if path:
+                res.append(path + [n])
+
+            for i in range(idx,int(math.sqrt(n))+1):
+                if n%i == 0:
+                    backtrack(n//i,i,path+[i])
+
+        res = []
+        backtrack(n,2,[])
+        return res
