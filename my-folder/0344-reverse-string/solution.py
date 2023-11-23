7d2061b6b0b09d4cd343b71c6fb051f09718c991
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-11-23 14:38:04 +0000

    [LeetCode Sync] Runtime - 193 ms (5.93%), Memory - 20.7 MB (93.51%)

--- my-folder/0344-reverse-string/solution.py
+++ my-folder/0344-reverse-string/solution.py
@@ -3,10 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left, right = left +1, right-1
+        i=0
+        j=len(s) -1
+        while i<j:
+            s[i], s[j] = s[j],s[i]
+            i +=1
+            j -= 1
         
-
+        
