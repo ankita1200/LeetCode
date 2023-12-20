14b61cc4edc08a8b6c86c64cdc7706c69f91f7f1
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-12-20 08:47:32 +0000

    [LeetCode Sync] Runtime - 114 ms (52.07%), Memory - 20.3 MB (6.26%)

---
+++ my-folder/1254-deepest-leaves-sum/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([root])
+        while queue:
+            no_of_nodes_level = len(queue)
+            sum=0
+            for _ in range(no_of_nodes_level):
+                node = queue.popleft()
+                sum+=node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return sum
+        
