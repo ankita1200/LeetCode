ca9134e62d793b4da6a4297ddbe476f0fd906fe0
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2023-12-29 06:31:40 +0000

    [LeetCode Sync] Runtime - 108 ms (58.69%), Memory - 17.4 MB (6.95%)

---
+++ my-folder/3193-maximum-strong-pair-xor-i/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumStrongPairXor(self, nums: List[int]) -> int:
+        nums.sort()
+
+        n = len(nums)
+
+        l = 0
+        r = 0
+        
+        res = 0
+
+        while r < n:
+            x = nums[l]
+            y = nums[r]
+
+            if y - x > x:
+                l += 1
+                continue
+
+            for i in range(l, r):
+                res = max(res, nums[i] ^ y)
+
+            r += 1
+
+        return res
