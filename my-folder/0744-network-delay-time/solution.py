d8cf635adef8d23e2031a3d7019d721f4f7688a7
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-05-17 08:36:21 +0000

    [LeetCode Sync] Runtime - 362 ms (79.67%), Memory - 18.6 MB (38.95%)

--- my-folder/0744-network-delay-time/solution.py
+++ my-folder/0744-network-delay-time/solution.py
@@ -1,32 +1,31 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         
+        #represent graph as adjacency list for easy access to neighbor vertices
         graph=defaultdict(list)
         for u,v,w in times:
             graph[u].append((w,v))
         
+        #use visited array to keep track of visited and non-visited nodes
         visited=[False]*(n+1)
         visited[0]=True
+        
+        #use an array to keep track of minimum distance to the nodes
         min_dist=[float('inf')]*(n+1)
-        pq=[]
         min_dist[0]=min_dist[k]=0
-        for node in graph[k]:
-            dist,dest=node
-            pq.append(node)
-            min_dist[dest]=dist
-        heapq.heapify(pq)
-        visited[k]=True
-        count=n-1
-        while pq and count>0:
+        
+        #use a minimum heap ds to get the next vertex with minimum distance at each iterration
+        pq=[(0,k)]
+        while pq:
             dist,node=heapq.heappop(pq)
             if not visited[node]:
-                visited[node]=True
                 for nx_node in graph[node]:
                     dest_dist, dest_node=nx_node
                     if not visited[dest_node]:
                         min_dist[dest_node]=min(min_dist[dest_node],dist+dest_dist)
                         heapq.heappush(pq,(min_dist[dest_node], dest_node))
-                count-=1
+                visited[node]=True
+                
         return max(min_dist) if float('inf') not in min_dist else -1
         
         
