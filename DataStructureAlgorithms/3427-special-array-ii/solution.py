0aa8b03dee4775eee7d8668634f0605b8de476fb
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-07-27 06:54:14 +0000

    [LeetCode Sync] Runtime - 994 ms (27.09%), Memory - 55.9 MB (33.42%)

---
+++ DataStructureAlgorithms/3427-special-array-ii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        B = [i for i in range(1, len(nums)) if nums[i]%2==nums[i-1]%2]
+        ans=[]
+        
+        for q in queries:
+            
+            L = bisect_right(B, q[0])
+            
+            R = bisect_right(B, q[1])
+        
+            if R-L==0:
+                ans.append(True)
+            else:
+                ans.append(False)
+            
+        return ans
