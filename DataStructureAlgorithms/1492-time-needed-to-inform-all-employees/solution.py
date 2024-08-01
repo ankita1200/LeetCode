4b670cb4d82d88de3bb738f0ddc1cc45828765c4
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-08-01 19:51:15 +0000

    [LeetCode Sync] Runtime - 971 ms (52.18%), Memory - 53.8 MB (29.62%)

---
+++ DataStructureAlgorithms/1492-time-needed-to-inform-all-employees/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        if len(manager)==1:
+            return 0
+        graph=defaultdict(list)
+        for index,emp in enumerate(manager):
+            if emp!=-1:
+                graph[emp].append(index)
+        
+        def dfs(node,val):
+            if not graph[node]:
+                self.maxval=max(self.maxval,val)
+            for neighbor in graph[node]:
+                dfs(neighbor,val+informTime[node])
+            return
+            
+        
+        self.maxval=0
+        dfs(headID,0)
+        return self.maxval
