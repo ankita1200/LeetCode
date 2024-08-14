8bc587040cafab60b7bfd57165d823c85040d0c9
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-08-14 10:17:02 +0000

    [LeetCode Sync] Runtime - 354 ms (86.78%), Memory - 16.9 MB (70.53%)

--- DataStructureAlgorithms/2206-detonate-the-maximum-bombs/solution.py
+++ DataStructureAlgorithms/2206-detonate-the-maximum-bombs/solution.py
@@ -1,36 +1,36 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        
         graph=defaultdict(list)
         n=len(bombs)
         for i in range(n):
+            a1,b1,r1 = bombs[i]
             for j in range(i+1,n):
-                dist=(bombs[i][0]-bombs[j][0])**2 + (bombs[i][1]-bombs[j][1])**2
-                if bombs[i][2]**2 >= dist:
+                a2,b2,r2 = bombs[j]
+                dist = (a1-a2)**2 + (b1-b2)**2
+                if dist <= r1**2:
                     graph[i].append(j)
-                if bombs[j][2]**2 >= dist:
+                if dist <= r2**2:
                     graph[j].append(i)
         
-        def bfs(bomb_index):
-            queue=deque([bomb_index])
+        def bfs(bomb):
             count=0
-            seen=set()
-            seen.add(bomb_index)
-            while queue:
-                index=queue.popleft()
-                count+=1
-                for neighbor in graph[index]:
-                    if not neighbor in seen:
-                        seen.add(neighbor)
-                        queue.append(neighbor)
-                        
+            seen = set()
+            que = deque([bomb])
+            seen.add(bomb)
+            while que:
+                size = len(que)
+                for _ in range(size):
+                    bombindex = que.popleft()
+                    count+=1
+                    for neighbor in graph[bombindex]:
+                        if neighbor not in seen:
+                            seen.add(neighbor)
+                            que.append(neighbor)
             return count
         
         ans=0
-        for index in range(n):
-            ans=max(ans,bfs(index))
+        for i in range(len(bombs)):
+            ans=max(ans,bfs(i))
         return ans
-
-        
-
-
         
