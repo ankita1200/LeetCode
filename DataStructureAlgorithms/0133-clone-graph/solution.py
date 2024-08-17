89a035a0598b456fb334aa12334128179f08f87c
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-08-17 09:17:36 +0000

    [LeetCode Sync] Runtime - 34 ms (90.48%), Memory - 17 MB (41.24%)

--- DataStructureAlgorithms/0133-clone-graph/solution.py
+++ DataStructureAlgorithms/0133-clone-graph/solution.py
@@ -8,21 +8,25 @@ class Node:
 
 from typing import Optional
 class Solution:
-
-    def __init__(self):
-        self.visited = {}
-
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return node
-        
-        if node in self.visited:
-            return self.visited[node]
-        
-        clone_node = Node(node.val,[])
-        self.visited[node] = clone_node
 
-        clone_node.neighbors=[self.cloneGraph(n) for n in node.neighbors]
-
-        return clone_node
+        def dfs(node):
+            if not node:
+                return
+            newnode = Node(node.val,[])
+            dicnodes[node.val] = newnode
+            for neighbor in node.neighbors:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    newnode.neighbors.append(dfs(neighbor))
+                else:
+                    newnode.neighbors.append(dicnodes[neighbor.val])
+            return newnode
+            
+        seen = set()
+        dicnodes = {}
+        seen.add(node)
+        return dfs(node)
+                    
+        
         
