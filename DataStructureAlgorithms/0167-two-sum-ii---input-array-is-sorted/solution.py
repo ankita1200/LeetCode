87d1034f058c94c47a01e0a3ddf57d089b9e4800
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-08-23 04:52:37 +0000

    [LeetCode Sync] Runtime - 103 ms (67.47%), Memory - 17.8 MB (40.94%)

---
+++ DataStructureAlgorithms/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+        
+        while i<j:
+            currsum = numbers[i] + numbers[j]
+            if currsum == target:
+                return [i+1,j+1]
+            if currsum < target:
+                i +=1
+            else:
+                j -= 1
+        
+        
