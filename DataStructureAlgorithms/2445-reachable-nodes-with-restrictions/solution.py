32761cc26d24bc75f70b0b3ead6c3cf875a2f207
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-09-12 09:55:57 +0000

    [LeetCode Sync] Runtime - 1301 ms (41.31%), Memory - 74.4 MB (76.32%)

--- DataStructureAlgorithms/2445-reachable-nodes-with-restrictions/solution.py
+++ DataStructureAlgorithms/2445-reachable-nodes-with-restrictions/solution.py
@@ -1,19 +1,23 @@
+from collections import defaultdict
 class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
-        graph=defaultdict(list)
+        
+        graph = defaultdict(list)
         for a,b in edges:
             graph[a].append(b)
             graph[b].append(a)
-        
+        seen = [False] * n
+        lookup = set(restricted)
+
         def dfs(node):
-            if node in seen:
-                return 0
-            seen.add(node)
-            return 1 + sum([dfs(neighbor) for neighbor in graph[node]])
-                
-        seen=set(restricted)
-        return dfs(0)
+            if seen[node] or node in lookup:
+                return
+            seen[node] = True
+            for neighbor in graph[node]:
+                dfs(neighbor)
         
-                
+        dfs(0)
+        return sum(seen)
+            
+
         
-                    
