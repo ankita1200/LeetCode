e62431154d394bf1d693cf15e0f9feb6206fd5fa
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-09-15 16:17:46 +0000

    [LeetCode Sync] Runtime - 361 ms (96.13%), Memory - 16.9 MB (90.10%)

--- DataStructureAlgorithms/1171-shortest-path-in-binary-matrix/solution.py
+++ DataStructureAlgorithms/1171-shortest-path-in-binary-matrix/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return
-        if grid[0][0]==1:
-            return -1
+        
         n = len(grid)
+        if not grid or grid[0][0]==1:
+            return -1
+        dirs = [(0,1),(1,0),(-1,0),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1)]
         que = deque([(0,0,1)])
-        seen = {(0,0)}
-        dirs = [(1,0),(0,1),(-1,0),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1)]
+        grid[0][0]=1
         while que:
-            row,col,steps = que.popleft()
-            if (row==n-1) and (col==n-1):
-                return steps
-            for dx,dy in dirs:
-                nrow = row+dx
-                ncol = col+dy
-                if (0<=nrow<n) and (0<=ncol<n) and ((nrow,ncol) not in seen) and (grid[nrow][ncol]==0):
-                    que.append((nrow,ncol,steps+1))
-                    seen.add((nrow,ncol))
+            size = len(que)
+            for _ in range(size):
+                x,y,dist = que.popleft()
+                if x==n-1 and y==n-1:
+                    return dist
+                for dx,dy in dirs:
+                    nx,ny = x+dx,y+dy
+                    if (0<=nx<n) and (0<=ny<n) and grid[nx][ny]==0:
+                        grid[nx][ny]=1
+                        que.append((nx,ny,dist+1))
+                
         return -1
-
-
-
+                        
+                
+                
+        
         
