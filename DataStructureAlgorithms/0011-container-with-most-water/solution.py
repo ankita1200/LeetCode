fbbbee5aaf7dd84cc4727dc68fc5b098d7d4356c
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-09-16 12:51:36 +0000

    [LeetCode Sync] Runtime - 549 ms (9.04%), Memory - 30 MB (24.37%)

--- DataStructureAlgorithms/0011-container-with-most-water/solution.py
+++ DataStructureAlgorithms/0011-container-with-most-water/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l1=0
-        n=len(height)
-        l2=n-1
-        area = 0
-        while l1 < l2:
-            a = (l2-l1) * min(height[l1], height[l2])
-            if height[l1] < height[l2]:
-                l1 +=1
+        
+        left = 0
+        right = len(height)-1
+        maxarea = 0
+        while left < right:
+            curr = (right-left)*min(height[left],height[right])
+            maxarea = max(maxarea,curr)
+            if height[right] < height[left]:
+                right -= 1
             else:
-                l2 -=1
-            area = max(a,area)
-
-        return area   
+                left += 1
+        return maxarea
+            
