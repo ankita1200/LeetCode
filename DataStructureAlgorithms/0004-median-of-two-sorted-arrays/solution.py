f85bbb7acc9abb94ca3de0caa60cd5496c8355cf
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-09-17 18:13:37 +0000

    [LeetCode Sync] Runtime - 77 ms (76.84%), Memory - 16.9 MB (63.42%)

--- DataStructureAlgorithms/0004-median-of-two-sorted-arrays/solution.py
+++ DataStructureAlgorithms/0004-median-of-two-sorted-arrays/solution.py
@@ -1,35 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        i = j = 0
-        m = len(nums1)
-        n = len(nums2)
-        if m == 0 and n == 0:
-            return 
-        if m == 0:
-            return nums2[int(n/2)] if n % 2 !=0 else (nums2[int(n/2)]+nums2[int(n/2-1)])/2
-        if n == 0:
-            return nums1[int(m/2)] if m % 2 !=0 else (nums1[int(m/2)]+nums1[int(m/2-1)])/2
-        fin = []
-        while i < m and j < n:
-            if nums1[i] < nums2[j]:
-                fin.append(nums1[i])
-                i +=1
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n2 < n1:
+             nums1, nums2, n1, n2 = nums2, nums1, n2, n1
+        low = 0
+        high = len(nums1)
+        while low <= high:
+            cut1 = (low + high) // 2
+          
+            cut2 = (n1+n2)//2 - cut1
+            
+            l1 = float('-inf') if cut1==0 else nums1[cut1-1]
+            l2 = float('-inf') if cut2==0 else nums2[cut2-1]
+            r1 = float('inf') if cut1==n1 else nums1[cut1]
+            r2 = float('inf') if cut2==n2 else nums2[cut2]
+            if l1 > r2:
+                high = cut1 - 1
+            elif l2 > r1:
+                low = cut1 + 1
             else:
-                fin.append(nums2[j])
-                j +=1
-        while i < m:
-            fin.append(nums1[i])
-            i += 1
-        while j < n:
-            fin.append(nums2[j])
-            j += 1
+                if (n1+n2) % 2 == 0:
+                    return (max(l1,l2)+min(r1,r2))/2
+                else:
+                    return min(r1,r2)
         
-        if (m+n)%2 == 0:
-            mid1 = (m+n)/2
-            mid2 = mid1-1
-            return (fin[int(mid1)]+fin[int(mid2)])/2
-        else:
-            return fin[int((m+n-1)/2)]
-                
-                
+
+
         
