019074e10980a6ea310e0e7eb77fc26fdf4a69e2
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-10-07 16:50:51 +0000

    [LeetCode Sync] Runtime - 44 ms (8.59%), Memory - 16.9 MB (6.34%)

--- DataStructureAlgorithms/0062-unique-paths/solution.py
+++ DataStructureAlgorithms/0062-unique-paths/solution.py
@@ -1,8 +1,12 @@
 class Solution:
-
-    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 1 or n == 1:
-            return 1
         
-        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        @lru_cache
+        def dp(row,col):
+            if row<0 or col<0 or row>=m or col>=n:
+                return 0
+            if row==m-1 and col==n-1:
+                return 1
+            return dp(row+1,col)+dp(row,col+1)
+        
+        return dp(0,0)
