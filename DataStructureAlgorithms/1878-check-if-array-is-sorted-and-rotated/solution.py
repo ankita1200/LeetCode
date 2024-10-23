ec2b06e03ec4c33f9f7128ad6479d2112227bd91
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-10-23 12:39:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.4 MB (90.63%)

---
+++ DataStructureAlgorithms/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = 0
+        for i in range(1,n):
+            if nums[i-1]>nums[i]:
+                count += 1
+        if count > 1 or (count==1 and nums[0]<nums[-1]):
+            return False
+        return True
+        
