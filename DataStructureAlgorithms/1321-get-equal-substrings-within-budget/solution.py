dd90f9d5f86afcec9cb28ecffece536df7b42027
ankita1200 <30814153+ankita1200@users.noreply.github.com>
2024-10-28 11:39:16 +0000

    [LeetCode Sync] Runtime - 36 ms (15.24%), Memory - 17.2 MB (72.90%)

--- DataStructureAlgorithms/1321-get-equal-substrings-within-budget/solution.py
+++ DataStructureAlgorithms/1321-get-equal-substrings-within-budget/solution.py
@@ -1,18 +1,20 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        left = cost = total_cost = answer = 0
-        curr = []
-
+        left = 0
+        currcost = 0
+        ans = 0
         for right in range(len(s)):
-            cost = abs(ord(s[right]) - ord(t[right]))
-            curr.append(cost)
-
-            total_cost += cost
-
-            while total_cost > maxCost:
-                total_cost -= curr[left]
+            if s[right]!=t[right]:
+                currcost += abs(ord(s[right])-ord(t[right]))
+            
+            while (currcost > maxCost) and left<=right:
+                if s[left] != t[left]:
+                    currcost -= abs(ord(s[left])-ord(t[left]))
                 left += 1
+            
+            ans = max(ans,right-left+1)
 
-            answer = max(answer, right - left + 1)
+        return ans    
 
-        return answer
+
+
